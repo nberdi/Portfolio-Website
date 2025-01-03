3f1b468ff198830785288b4c493bc9b21316f639
Nauka <berdinauryzbek@gmail.com>
2025-01-03 06:15:53 -0500

    added btns for project section with links

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -19,8 +19,8 @@ const Project = () => {
               <a href={project.link}>
                 <img
                   src={project.image}
-                  width={272}
-                  height={272}
+                  width={320}
+                  height={320}
                   alt={project.title}
                   className="img"
                 />
@@ -32,6 +32,42 @@ const Project = () => {
               {project.technologies.map((tech, index) => (
                 <span key={index} className="project_tech">
                   {tech}
+                </span>
+              ))} <br /><br />
+              {project.btn.map((btn, index) => (
+                <span key={index}>
+                  {/* From Uiverse.io by adamgiebl - Smaller Version */}
+                  <button className="cssbuttons-io">
+                    <span>
+                      {index === 0 && (
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M0 0h24v24H0z" fill="none"></path>
+                          <path
+                            d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      )}
+                      {index === 1 && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                        >
+                          <path d="M0 0h24v24H0z" fill="none" />
+                          <path d="M8 5v14l11-7z" />
+                        </svg>
+                      )}
+                      <a
+                        href={btn.link} // Dynamically set the link
+                        target="_blank" // Open in a new tab
+                        rel="noopener noreferrer" // Security best practice
+                        style={{ textDecoration: 'none', color: 'white' }} // Optional: Remove underline for the link
+                      >{btn.text}</a>
+                    </span>
+                  </button>
                 </span>
               ))}
             </div>
